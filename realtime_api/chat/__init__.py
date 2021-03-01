2f331b2db4ef6b51ffa64669a261e2145ca49388
Mārtiņš Mačs <macs.martins@gmail.com>
2021-03-01 07:26:08 +0200

    private messages

--- realtime_api/chat/__init__.py
+++ realtime_api/chat/__init__.py
@@ -1,1 +1,1 @@
-from .chat import chat_join, chat_part, chat_send
+from .chat import chat_join, chat_part, chat_send, pm_send
--- realtime_api/chat/chat.py
+++ realtime_api/chat/chat.py
@@ -1,5 +1,10 @@
+import collections
+import dataclasses
+import random
 import time
+import typing
 
+from ogs_api.rest_api.download import access_api
 from ogs_api.realtime_api.socket import comm_socket
 
 """
@@ -37,6 +42,37 @@ def chat_send(channel: str, message: str):
     comm_socket.emit("chat/send", data={
         'channel': channel,
         'uuid': _n2s(_user_id) + '.' + _n2s(int(time.time() * 1000)),
+        'message': message,
+    })
+
+
+
+_name2id_cache: typing.Dict[str, int] = dict()
+
+@dataclasses.dataclass
+class PrivateChat:
+    base: str
+    number: int = 0
+
+_private_chat_cache: typing.Dict[int, PrivateChat] = \
+    collections.defaultdict(lambda: PrivateChat(_n2s(random.randint(0, 1000))))
+
+def pm_send(username: str, message: str):
+    try:
+        player_id = _name2id_cache[username]
+    except KeyError:
+        results = access_api('/api/v1/players', params={'username': username})
+        assert results['count'] == 1
+        player_id = results['results'][0]['id']
+        _name2id_cache[username] = player_id
+
+    private_chat = _private_chat_cache[player_id]
+    private_chat.number += 1
+
+    comm_socket.emit('chat/pm', {
+        'player_id': player_id,
+        'username': username,
+        'uid': private_chat.base + '.' + _n2s(private_chat.number),
         'message': message,
     })
 
